6bd86b9abb9fc7a544cbb88362c942c55cabaa79
Cell6969 <bossmarinoo@gmail.com>
2025-08-16 17:06:46 +0700

    change: api tag for shipment

--- app/api/router.py
+++ app/api/router.py
@@ -3,29 +3,29 @@ from fastapi import APIRouter
 from app.api.dependencies import ServiceDepends
 from app.api.schemas.schemas import ShipmentCreate, ShipmentResponse, ShipmentUpdate, ShipmentUpdatePartial
 
-router = APIRouter()
+router = APIRouter(prefix="/shipment", tags=["shipment"])
 
-@router.get("/shipment", response_model=list[ShipmentResponse])
+@router.get("/", response_model=list[ShipmentResponse])
 async def get_shipment(service:ServiceDepends):
     return await service.list()
 
-@router.post("/shipment", response_model=ShipmentResponse)
+@router.post("/", response_model=ShipmentResponse)
 async def submit_shipment(body:ShipmentCreate, service:ServiceDepends):
     return await service.add(body)
 
-@router.get("/shipment/{id}", response_model=ShipmentResponse)
+@router.get("/{id}", response_model=ShipmentResponse)
 async def get_shipment_by_id(id:int, service:ServiceDepends):
     return await service.get(id)
 
-@router.put("/shipment/{id}", response_model=ShipmentResponse)
+@router.put("/{id}", response_model=ShipmentResponse)
 async def update_shipment(id:int, body:ShipmentUpdate, service:ServiceDepends):
     return await service.update(id, body)
 
-@router.patch("/shipment/{id}", response_model=ShipmentResponse)
+@router.patch("/{id}", response_model=ShipmentResponse)
 async def patch_shipment(id:int, body:ShipmentUpdatePartial, service:ServiceDepends):
     return await service.update_partial(id, body)
 
-@router.delete("/shipment/{id}")
+@router.delete("/{id}")
 async def delete_shipment(id:int, service:ServiceDepends) -> dict[str, str]:
     await service.delete(id)
     return {"message": f"shipment with id {id} deleted"}
