62455cdc689565f316249e2bbfbc59cb1476e02d
Cell6969 <bossmarinoo@gmail.com>
2025-08-30 15:58:03 +0700

    add : manual http token bearer

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,29 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer, HTTPBearer
+
+from app.utils import decode_access_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/seller/token")
+
+
+class AccessTokenBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth_credentials = await super().__call__(request)
+
+        if auth_credentials is None or not auth_credentials.credentials:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Invalid or missing token"
+            )
+        token_data = decode_access_token(auth_credentials.credentials)
+
+        if token_data is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Not Authorized"
+            )
+
+        return token_data
+
+
+access_token_bearer = AccessTokenBearer()
+Annotated[dict, Depends(access_token_bearer)]
